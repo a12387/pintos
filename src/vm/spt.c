0e5ad92d30ef490739c217750893d2401a252c0d
a12387 <1364219910@qq.com>
2025-05-10 14:22:20 +0000

    Project 3b complete

--- src/vm/spt.c
+++ src/vm/spt.c
@@ -13,13 +13,15 @@ void spt_free(struct spt *s) {
 
   int old_ofs = file_tell(s->file);
   for(int i = 0; i < nbytes; i += PGSIZE) {
-    void *uaddr = (void *)s->start_uaddr + (i * PGSIZE);
+    void *uaddr = (void *)s->start_uaddr + i;
     uint32_t *pte = lookup_page(thread_current()->pagedir, uaddr, false);
     if (pte != NULL) {
       if (*pte & PTE_P) {
         if (*pte & PTE_D) {
-          int size_to_write = (nbytes - i) > PGSIZE ? PGSIZE : (nbytes - i);
-          file_seek(s->file, s->offset + i * PGSIZE);
+          int size_to_write = nbytes - i;
+          if (size_to_write > PGSIZE)
+            size_to_write = PGSIZE;
+          file_seek(s->file, s->offset + i);
           file_write(s->file, uaddr, size_to_write);
         } 
         void *pa = pte_get_page(*pte);
